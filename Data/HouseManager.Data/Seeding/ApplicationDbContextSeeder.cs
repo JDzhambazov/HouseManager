8fb326af6d19dbfea84a6e29d929280e5dcb2493
JDzhambazov <jamby@mail.bg>
2021-05-13 10:40:46 +0300

    Create AddressSeeder

--- Data/HouseManager.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/HouseManager.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -27,6 +27,7 @@
                           {
                               new RolesSeeder(),
                               new SettingsSeeder(),
+                              new AddressSeeder(),
                           };
 
             foreach (var seeder in seeders)
