d36ea671ea3d79769385cb3065da39c3b386a375
JDzhambazov <jamby@mail.bg>
2021-05-13 13:30:05 +0300

    Property Name not required

--- Web/HouseManager.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/HouseManager.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,7 +52,6 @@
             [Display(Name = "Потребителско Име")]
             public string UserName { get; set; }
 
-            [Required]
             [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
             [Display(Name = "Име")]
             public string FirsName { get; set; }
